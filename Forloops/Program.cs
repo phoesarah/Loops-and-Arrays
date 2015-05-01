910bb1ea6dacd62b86a8858af8a50b613068bf5c
Sarah R <phoesarah@hotmail.com>
2015-04-30 19:02:10 -0500

    Wrote loop guessing game

---
+++ Forloops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forloops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int actualNumber = rand.Next(1, 10);
+            Console.WriteLine("Please guess a number between 1 and 10");
+            string guess;
+
+
+            for (int i = 2; i >= 0; i--)
+            {
+              guess = Console.ReadLine();
+                if (guess == actualNumber.ToString())
+                {
+                    Console.WriteLine("You Win");
+                    break;
+                }
+                if (i == 0)
+                {
+                    Console.WriteLine("You're a Loser, the number was {0} ", actualNumber);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! You have {0} guess{1} left", i, i != 1 ? "es" : string.Empty);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
