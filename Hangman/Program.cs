7f33389c8f001391d2ff4edeb7af52fb6a2c2cb1
Sarah R <phoesarah@hotmail.com>
2015-05-01 16:27:50 -0500

    add hangman

---
+++ Hangman/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string word = "Guess";
+            char[] actualletters = word.ToCharArray();
+
+            string guess = Console.ReadLine();
+
+            //char[] letterguess = guess.ToCharArray();
+
+           
+
+        }
+    }
+}
